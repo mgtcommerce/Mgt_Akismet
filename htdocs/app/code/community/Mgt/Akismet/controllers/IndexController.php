9875bc72ea8dc44ec47bb6bd062db5bfa1b1a9fe
swieczorek <stefan.wieczorek@weemedia.de>
2012-07-09 23:24:33 +0200

    spam check for contact form

---
+++ htdocs/app/code/community/Mgt/Akismet/controllers/IndexController.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * MGT-Commerce GmbH
+ * http://www.mgt-commerce.com
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@mgt-commerce.com so we can send you a copy immediately.
+ *
+ * @category    Mgt
+ * @package     Mgt_Akismet
+ * @author      Stefan Wieczorek <stefan.wieczorek@mgt-commerce.com>
+ * @copyright   Copyright (c) 2012 (http://www.mgt-commerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Mgt Akismet Contacts Controller
+ *
+ * @category   Mgt
+ * @package    Mgt_Akismet
+ * @author     Stefan Wieczorek <stefan.wieczorek@mgt-commerce.com>
+ */
+ 
+require_once 'Mage/Contacts/controllers/IndexController.php';
+
+class Mgt_Akismet_IndexController extends Mage_Contacts_IndexController
+{
+    public function postAction()
+    {
+        $post = $this->getRequest()->getPost();
+        if ( $post ) {
+            $translate = Mage::getSingleton('core/translate');
+            /* @var $translate Mage_Core_Model_Translate */
+            $translate->setTranslateInline(false);
+            try {
+                $postObject = new Varien_Object();
+                $postObject->setData($post);
+    
+                $error = false;
+    
+                if (!Zend_Validate::is(trim($post['name']) , 'NotEmpty')) {
+                    $error = true;
+                }
+    
+                if (!Zend_Validate::is(trim($post['comment']) , 'NotEmpty')) {
+                    $error = true;
+                }
+    
+                if (!Zend_Validate::is(trim($post['email']), 'EmailAddress')) {
+                    $error = true;
+                }
+    
+                if (Zend_Validate::is(trim($post['hideit']), 'NotEmpty')) {
+                    $error = true;
+                }
+                
+                $akismet = Mage::getSingleton('mgt_akismet/akismet');
+                if ($akismet->isActive()) {
+                    $data = array(
+                        'name'    => $postObject->getName(),
+                        'email'   => $postObject->getEmail(),
+                        'comment' => $postObject->getComment(),
+                    );
+                    if ($akismet->isSpam($data)) {
+                        $error = true;
+                    }
+                }
+
+                if ($error) {
+                    throw new Exception();
+                }
+                $mailTemplate = Mage::getModel('core/email_template');
+                /* @var $mailTemplate Mage_Core_Model_Email_Template */
+                $mailTemplate->setDesignConfig(array('area' => 'frontend'))
+                ->setReplyTo($post['email'])
+                ->sendTransactional(
+                        Mage::getStoreConfig(self::XML_PATH_EMAIL_TEMPLATE),
+                        Mage::getStoreConfig(self::XML_PATH_EMAIL_SENDER),
+                        Mage::getStoreConfig(self::XML_PATH_EMAIL_RECIPIENT),
+                        null,
+                        array('data' => $postObject)
+                );
+    
+                if (!$mailTemplate->getSentSuccess()) {
+                    throw new Exception();
+                }
+    
+                $translate->setTranslateInline(true);
+    
+                Mage::getSingleton('customer/session')->addSuccess(Mage::helper('contacts')->__('Your inquiry was submitted and will be responded to as soon as possible. Thank you for contacting us.'));
+                $this->_redirect('*/*/');
+    
+                return;
+            } catch (Exception $e) {
+                $translate->setTranslateInline(true);
+    
+                Mage::getSingleton('customer/session')->addError(Mage::helper('contacts')->__('Unable to submit your request. Please, try again later'));
+                $this->_redirect('*/*/');
+                return;
+            }
+    
+        } else {
+            $this->_redirect('*/*/');
+        }
+    }
+}
